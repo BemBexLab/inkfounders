19ed15618e278a46b2b554405690c22833c008b2
car5p3 <triggred47@icloud.com>
2026-03-03 10:51:45 -0800

    Updated metatag in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,53 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer2";
 import DynamicPageTitle from "@/components/DynamicPageTitle";
 import { workSans } from "./fonts";
 
-
-
-
-
 export const metadata: Metadata = {
   title: "Ink Founders",
-  description: "Ink Founders is an independent self-publishing platform dedicated to empowering authors with a full suite of digital services.",
+  description:
+    "Ink Founders is an independent self-publishing platform dedicated to empowering authors with a full suite of digital services.",
+  verification: {
+    google: "chIy7we8dy-XbVv1lNa36kZNuZgH_HCtWnDiGBB-uFA",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <head>
-        {/* Meta Pixel Code */}
-        <script
+      <body
+        className={`antialiased ${workSans.className}`}
+        suppressHydrationWarning
+      >
+        {/* Meta Pixel Script */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1969070570295864');
-fbq('track', 'PageView');
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)n=f.fbq;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}
+              (window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1969070570295864');
+              fbq('track', 'PageView');
             `,
           }}
         />
+
+        {/* Meta Pixel Noscript Fallback */}
         <noscript>
           <img
             height="1"
@@ -48,12 +57,7 @@ fbq('track', 'PageView');
             alt=""
           />
         </noscript>
-        {/* End Meta Pixel Code */}
-      </head>
-      <body
-        className={`antialiased ${workSans.className}`}
-        suppressHydrationWarning
-      >
+
         <DynamicPageTitle />
         <Header />
         {children}
