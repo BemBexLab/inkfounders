1b630732d5737f836636f23c42a771d432e5bbb9
car5p3 <triggred47@icloud.com>
2026-04-28 10:08:39 -0700

    Updated Neew route testing

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingNavbar2 from './components/LandingNavbar2'
+
+const page = () => {
+  return (
+    <section>
+        <LandingNavbar2 />
+    </section>
+  )
+}
+
+export default page
