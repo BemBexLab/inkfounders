2e297a2359732dce7b99b4db1d6826165d4590c6
car5p3 <triggred47@icloud.com>
2026-09-05 00:35:37 +0500

    Updated: Added Books New

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,12 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60 * 60 * 24 * 30,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
   },
 };
 
